83c3afb7ca71e2b8d89ac1f647d234de6d9a3898
Ko <rhxodn011020@naver.com>
2025-08-28 18:28:57 +0900

    Update Baekjoon-1253_좋다.java

--- Taewoo/Aug/Baekjoon-1253_좋다.java
+++ Taewoo/Aug/Baekjoon-1253_좋다.java
@@ -1,23 +1,61 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		StringBuilder sb = new StringBuilder();
-		Scanner sc = new Scanner(System.in);
-		
-		int n = sc.nextInt();
-		sc.nextLine();
-		String number = sc.nextLine();
-		
-		int sum = 0;
-		int num = 0;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static StringTokenizer st;
 
-		for (int i = 0; i<number.length(); i ++) {
-			char ch = number.charAt(i);
-			num = ch - '0';
-			sum+=num;
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.valueOf(st.nextToken());
+
+		st = new StringTokenizer(br.readLine());
+		int[] num_arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			num_arr[i] = Integer.valueOf(st.nextToken());
 		}
 		
-		System.out.println(sum);
+		if (n < 3) {
+			bw.write("0");
+			bw.flush();
+			bw.close();
+			return;
+		}
+
+		Arrays.sort(num_arr);
+
+		int start, end, sum, count = 0;
+
+		for (int i = 0; i < n; i++) {
+			start = 0;
+			end = n - 1;
+
+			while (start < end) {
+				if (start == i) {
+					start++;
+					continue;
+				}
+				if (end == i) {
+					end--;
+					continue;
+				}
+
+				sum = num_arr[start] + num_arr[end];
+
+				if (sum == num_arr[i]) {
+					count++;
+					break;
+				} else if (sum > num_arr[i]) {
+					end--;
+				} else {
+					start++;
+				}
+			}
+		}
+
+		bw.write(String.valueOf(count));
+		bw.flush();
+		bw.close();
 	}
 }
